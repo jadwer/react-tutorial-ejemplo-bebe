e5765d5d38776290dcd799d33dd38adef451688b
Rodrigo G Ramirez M <jadwer@msn.com>
2022-07-12 21:52:50 -0500

    iniciando el ejemplo mas basico

---
+++ src/components/TopPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class TopPage extends Component {
+    render() {
+        return(
+            <header>
+                <h1>Probando el componente</h1>
+            </header>
+        )
+    }
+}
+export default TopPage
